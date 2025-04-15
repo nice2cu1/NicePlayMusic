33209c4c554ce0768122cb01553c1c3d36959814
nice2cu1 <nice2cu1@outlook.com>
2025-04-15 12:42:56 +0800

    更新 BannerMapper，添加歌曲封面和文件路径字段

--- src/main/java/com/nice2cu1/niceplaymusic/mapper/BannerMapper.java
+++ src/main/java/com/nice2cu1/niceplaymusic/mapper/BannerMapper.java
@@ -8,8 +8,8 @@ import java.util.Map;
 
 @Mapper
 public interface BannerMapper {
-    @Select("SELECT b.id AS banner_id, b.song_id, b.cover_url, b.banner_title, b.banner_description, b.lrc, " +
-            "s.title AS song_title, s.artist AS song_artist, s.genre AS song_genre " +
+    @Select("SELECT b.id AS banner_id, b.song_id,b.banner_cover_url, b.cover_url, b.banner_title, b.banner_description, b.lrc, " +
+            "s.title AS song_title, s.artist AS song_artist, s.genre AS song_genre, s.file_path, s.duration " +
             "FROM banner_songs b " +
             "JOIN songs s ON b.song_id = s.id")
     List<Map<String, Object>> getBannerSongs();
